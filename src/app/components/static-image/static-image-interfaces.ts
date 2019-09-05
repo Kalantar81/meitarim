8ff38b3ef752eaa9b027ca1eaf4bd591216ef962
Dmitry Calantarov <P0025363@ness-tech.co.il>
2019-09-05 13:23:06 +0300

    interfaces for static image

---
+++ src/app/components/static-image/static-image-interfaces.ts
@@ -0,0 +1,22 @@
+export interface IStaticImageOptions {
+  height: number;
+  width: number;
+  src: string;
+}
+
+export interface IImageStyle {
+  background: string;
+  display?: string;
+  width?: string;
+  height?: string;
+}
+
+export class SegmentParams {
+  segmentName: string;
+  createdBy: string;
+  date: Date;
+  startPointX: number;
+  endPointX: number;
+  startPointY: number;
+  endPointY: number;
+}
